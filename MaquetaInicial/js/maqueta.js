3fb282cc0ba8501f9079e7be2ed55df546b9908c
karinAAC <karin_alejo@hotmail.com>
2017-03-29 10:34:28 -0500

    subiendo pagina principal al master

---
+++ MaquetaInicial/js/maqueta.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',function() {
+
+  var registrate = document.getElementById("registrate");
+
+  registrate.addEventListener('click',function(e) {
+    e.preventDefault();
+    //window.location = "";
+    
+  });
+
+  var iniciar = document.getElementById("iniciar");
+  iniciar .addEventListener("click", function(e){
+    e.preventDefault();
+    //window.location = "";
+  });
+
+});
